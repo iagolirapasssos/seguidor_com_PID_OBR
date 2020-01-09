4b48e5716de7ec98d8dca17f2a71ba6cd35f99ce
iagolirapassos <iagolirapassos@gmail.com>
2020-01-09 15:37:43 -0300

    Melhorias nas curvas

--- curvas.h
+++ curvas.h
@@ -12,43 +12,44 @@ int lerSensoresCor()
   Serial.print(IR3);
   Serial.println("");
   
-  //Verde esquerdo
+//Verificar se foi detectado VERDE no lado ESQUERDO
   if(Erro2 > 8 && Erro2 < 11 && Erro1 < 13 && IR3 > 200)
   {
-    mover(0, 0);
-    pausa(400);
-    controle(1, 0, 0);
-    long Erro1 = erro1();
-    long Erro2 = erro2();
-    
-    if(Erro2 > 8 && Erro2 < 11 && Erro1 < 13 && IR3 > 200)
-    {  
-      Serial.println("Verde E1");
-      LedsAlerta(2, 150);
-      delay(500);
-      LedsAlerta(4, 0);
-      return 1;
-    }
+      mover(0, 0);
+      pausa(400);
+      controle(1, 0, 0);
+      long Erro1 = erro1();
+      long Erro2 = erro2();
+      
+        if(Erro2 > 8 && Erro2 < 11 && Erro1 < 13 && IR3 > 200)
+        {  
+          Serial.println("Verde E1");
+          LedsAlerta(2, 150);
+          delay(500);
+          LedsAlerta(4, 0);
+          return 1;
+        }
   }
-  //Verde direita
+//Verificar se foi detectado VERDE no lado DIREITO
   else if(Erro1 > 15 && Erro1 < 18 && Erro2 < 8  && IR3 > 200)
   {
-    mover(0, 0);
-    pausa(400);
-    controle(1, 0, 0);
-    long Erro1 = erro1();
-    long Erro2 = erro2();
-
-    mover(0, 0);
-    pausa(500);
-    if(Erro1 > 15 && Erro1 < 18 && Erro2 < 8  && IR3 > 200)
-    {
-      Serial.println("Verde D1");
-      LedsAlerta(2, 150);
-      delay(300);
-      LedsAlerta(4, 0);
-      return 2;
-    }
+      mover(0, 0);
+      pausa(400);
+      controle(1, 0, 0);
+      long Erro1 = erro1();
+      long Erro2 = erro2();
+  
+      mover(0, 0);
+      pausa(500);
+      
+        if(Erro1 > 15 && Erro1 < 18 && Erro2 < 8  && IR3 > 200)
+        {
+            Serial.println("Verde D1");
+            LedsAlerta(2, 150);
+            delay(300);
+            LedsAlerta(4, 0);
+            return 2;
+        }
   }
   //Curva 90 a esquerda
   else if(Erro2 > 50 && Erro2 < 380 && Erro1 < 11 && IR3 < 40)
@@ -56,11 +57,12 @@ int lerSensoresCor()
     mover(0, 0);
     pausa(400);
     controle(1, 0, 0);
+    mover(0, 0);
+    pausa(400);
+    
     long Erro1 = erro1();
     long Erro2 = erro2();
 
-    mover(0, 0);
-    pausa(500);
     if(Erro2 > 50 && Erro2 < 380 && Erro1 < 11 && IR3 < 40)
     {
       Serial.println("Curva 90 a esquerda");
@@ -75,11 +77,12 @@ int lerSensoresCor()
     mover(0, 0);
     pausa(400);
     controle(1, 0, 0);
+    mover(0, 0);
+    pausa(400);
+    
     long Erro1 = erro1();
     long Erro2 = erro2();
 
-    mover(0, 0);
-    pausa(500);
     if(Erro2 < 8 && Erro1 > 200 && Erro1 < 380 && IR3 < 40)
     {
       Serial.println("Curva 90 a direita");
@@ -94,11 +97,12 @@ int lerSensoresCor()
     mover(0, 0);
     pausa(400);
     controle(1, 0, 0);
+    mover(0, 0);
+    pausa(400);
+    
     long Erro1 = erro1();
     long Erro2 = erro2();
 
-    mover(0, 0);
-    pausa(500);
     if(Erro2 > 50 && Erro1 > 200 && IR3 > 300)
     {
       Serial.println("Encruzilhada");
@@ -113,11 +117,12 @@ int lerSensoresCor()
     mover(0, 0);
     pausa(400);
     controle(1, 0, 0);
+    mover(0, 0);
+    pausa(400);
+    
     long Erro1 = erro1();
     long Erro2 = erro2();
 
-    mover(0, 0);
-    pausa(500);
     if(Erro2 > 50 && Erro1 > 200 && IR3 < 200)
     {
       Serial.println("T");
@@ -194,19 +199,27 @@ int x = lerSensoresCor();
           pausa(250);
           break;
      case 11:
-        //Frente
+          //pção para CURVA em 90 GRAUS para ESQUERDA:
+          
+          //Pausa real de 500 ms
           mover(0, 0);
           pausa(500);
-
+       
+          //LOOP 1: Ir para frente enquanto a função não retorna 999
           while(1)
           {
             controle(1, 0, 10.0);
             if(lerSensoresCor() == 999) break;
           }
+          
+          //Pausa real de 500 ms
           mover(0, 0);
           pausa(500);
         
-        //Esquerda 90
+        /*LOOP 2: No loop 1 o robô foi deslocado para frente em x metros.
+         *Agora faremos com que o robô vire para a esquerda até que o 
+         *sensor do meio (IR3) detecte preto.
+         */
           while(1)
           {
             //PID(kP, kI, kD, PWM, media);
@@ -215,42 +228,43 @@ int x = lerSensoresCor();
             if(IR3 > 200) break;
             //if(lerSensoresCor() == 999) break;
           }
+          
           mover(0, 0);
           pausa(300);
-          
-//        //Ré
-//          mover(0, 0);
-//          pausa(250);
-//          controle(-1, 0, 2);
-//          mover(0, 0);
-//          pausa(250);
           break;
      case 22:
+     //pção para CURVA em 90 GRAUS para DIREITA:
+          
+          //Pausa real de 500 ms
           mover(0, 0);
           pausa(500);
-        //Frente
-          controle(1, 0, 10.0);
-          mover(0, 0);
-          pausa(300);
-            
-        //Direita 90
+       
+          //LOOP 1: Ir para frente enquanto a função não retorna 999
           while(1)
           {
+            controle(1, 0, 10.0);
+            if(lerSensoresCor() == 999) break;
+          }
+          
+          //Pausa real de 500 ms
+          mover(0, 0);
+          pausa(500);
+        
+        /*LOOP 2: No loop 1 o robô foi deslocado para frente em x metros.
+         *Agora faremos com que o robô vire para a direita até que o 
+         *sensor do meio (IR3) detecte preto.
+         */
+          while(1)
+          {
+            //PID(kP, kI, kD, PWM, media);
             controle(0, -1, 0.0);
             sensoresIR();
             if(IR3 > 200) break;
-            //PID(kP, kI, kD, PWM, media);
             //if(lerSensoresCor() == 999) break;
           }
+          
           mover(0, 0);
           pausa(300);
-          
-        //Ré
-          mover(0, 0);
-          pausa(250);
-          controle(-1, 0, 2);
-          mover(0, 0);
-          pausa(250);
           break;
      case 999:
           LedsAlerta(4, 150);
